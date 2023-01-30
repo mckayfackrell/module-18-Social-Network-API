074176c6de540daa49a7320dd7757b4b4a9755b5
McKay Fackrell <110206514+mckayfackrell@users.noreply.github.com>
2023-01-30 16:57:31 -0700

    added files from in class help, trying to understand how to fix

--- models/Reaction.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-
-
-
-
-module.exports = reaction;
--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ReactionSchema = require("./Reaction");
 
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a")
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ReactionSchema]
+});
 
+ThoughtSchema.virtual("reactionCount").get(function() {
+  return this.reactions.length;
+});
 
+const Thought = mongoose.model("Thought", ThoughtSchema);
 
-
-module.exports = thought;
+module.exports = Thought;
