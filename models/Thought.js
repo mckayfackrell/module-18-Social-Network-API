1212d0144b3bdc29cd643eebaf74fe076ac6a661
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-02 12:50:44 -0700

    added the reactionSchema and notes to the Thought.js file in models folder

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,70 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require('../utils/helpers');
+const dateFormat = require("../utils/helpers"); //import the dateFormate function from utils
 
-const ThoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280
+// Define the Reaction schema
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (Date) => dateFormat(Date),
+    },
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a")
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  reactions: [ReactionSchema]
-});
+  {
+    toJSON: {
+      getters: true,
+    },
+    // a custom getter is a function that can transform a value before it gets returned when calling toJSON method on a Mongoose document. By setting toJSON.getters to true, Mongoose will apply the getter functions before returning the JSON representation
+  }
+);
 
-ThoughtSchema.virtual("reactionCount").get(function() {
+// Define the Thought schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (Date) => dateFormat(Date),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+// Virtual property to return the number of reactions for each thought
+thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
-const Thought = mongoose.model("Thought", ThoughtSchema);
+// Create a Mongoose model for the Thought schema
+const Thought = model("Thought", thoughtSchema);
 
 module.exports = Thought;
