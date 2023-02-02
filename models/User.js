ad6b3eb7ee90ca0f25bd0c5f3a03c390382dff43
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-02 12:30:37 -0700

    added notes to User.js in the models folder

--- models/User.js
+++ models/User.js
@@ -1,45 +1,43 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      unique: true,
-      required: true,
-      trim: true,
+      unique: true, // the username must be unique in the database
+      required: true, // the username is a required field
+      trim: true, // removes any leading or trailing whitespace
     },
     email: {
       type: String,
-      required: [true, "Please enter Email Address"],
-      unique: true,
-      match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+      required: [true, "Please enter Email Address"], // the email must be provided and an error message is displayed if not
+      unique: true, //the email must be unique in the database
+      match: /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, // regex to match a valid email address
     },
     thoughts: [
       {
-        type: Schema.Types.ObjectId,
-        ref: "Thought",
+        type: Schema.Types.ObjectId, // this is a MongoDB object ID type
+        ref: "thought", // this refers to the Thought model
       },
     ],
     friends: [
       {
-        type: Schema.Types.ObjectId,
-        ref: "User",
+        type: Schema.Types.ObjectId, // this is a MongoDB object ID type
+        ref: "user", // this refers to the User model
       },
     ],
   },
   {
     toJSON: {
-      virtuals: true,
-    },
-    id: false,
+      virtuals: true, // this makes virtual properties (like friendCount) included in the JSON response
+    }
   }
 );
 
 userSchema.virtual("friendCount").get(function () {
-  return this.friends.length;
+  return this.friends.length; // this virtual property returns the number of friends a user has
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
