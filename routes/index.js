f3dd0cfcac9f759bbe776d8e8cb5f478de27a623
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-02 12:09:56 -0700

    added notes to index.js within routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+// import the Express.js Router function and the ./api module
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
+// mount the imported apiRoutes module at the path /api, meaning any routes defined in the apiRoutes module will be accessible under the /api path
 router.use("/api", apiRoutes);
 
+// catch-all route for requests to unknown routes
 router.use((req, res) => {
   return res.send("Wrong route!");
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,4 @@
+// takes a Date object as an argument and returns a string representation of the date and time
 function dateFormat(Date) {
   const dateString = Date.toLocaleSting();
   return dateString;
