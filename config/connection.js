3b60ba6b43d0c83969f7335cb668aebe12499ad8
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-02 12:24:19 -0700

    added notes to connectino.js file

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const { connect, connection } = require('mongoose');
 
+// sets a connection to either the MONGODB_URI environment variable if it's available, or to a local development database 
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
 
+// connects to a MongoDB database with the specified connection string
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
