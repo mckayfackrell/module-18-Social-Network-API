dd223e64a46211da88db5c4a0079fe80fa8b37db
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-02 12:35:04 -0700

    added notes to index.js file in models folder

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
+// import the User and Thought models
 const User = require('./User');
 const Thought = require('./Thought');
 
-
+// export the User and Thought models as a single object
 module.exports = { User, Thought };
