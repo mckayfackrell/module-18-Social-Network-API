329078111070eb52e307bef6c8f611f27bb4a1f8
“mckayfackrell” <mvfackrell@gmail.com>
2023-02-02 12:21:20 -0700

    added information and notes to the thoughtRoutes.js file

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const { Schema, model } = require("mongoose");
+const dateFormat = require('../utils/helpers');
 
 const ThoughtSchema = new Schema({
   thoughtText: {
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController')
+
+// route will be /api/thoughts to get all thoughts and create a new thought
+router.route('/').get(getThoughts).post(createThought);
+
+// the route for /:thoughtId will .get a single thought, .put (update) a single thought and .delete (delete) a single thought
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// the /:thoughtId/reactions will .post (create) a new reaction
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /:thoughtId/reactions/:reactionsId deletes the specified reaction with the id number in the route
+router.route('/:thoughtId/reactions/:reactionsId').delete(deleteReaction);
+
+module.exports = router;
